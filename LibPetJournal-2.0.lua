6b76c37126c38e6477c41cacb26251d35ab2b317
Robin Schoonover <robin@cornhooves.org>
2015-10-16 18:09:07 -0600

    Remove pre-6.0 client compat code.

--- LibPetJournal-2.0.lua
+++ LibPetJournal-2.0.lua
@@ -57,11 +57,6 @@ do
         [LE_PET_JOURNAL_FLAG_NOT_COLLECTED] = true,
     }
 
-    -- pre-WoD (<6.0)
-    if LE_PET_JOURNAL_FLAG_FAVORITES ~= nil then
-        PJ_FLAG_FILTERS[LE_PET_JOURNAL_FLAG_FAVORITES] = false
-    end
-
     local s_search_filter
     local flag_filters = {}
     local type_filters = {}
