e7d5ca522fc5df11d0d5e41578459a990e334d2f
Robin Schoonover <robin@cornhooves.org>
2015-10-16 18:17:55 -0600

    Bump minor.

--- LibPetJournal-2.0.lua
+++ LibPetJournal-2.0.lua
@@ -20,7 +20,7 @@
  IN THE SOFTWARE.
 ]]
 
-local MAJOR, MINOR = "LibPetJournal-2.0", 27
+local MAJOR, MINOR = "LibPetJournal-2.0", 28
 local lib, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 
 if not lib then return end
